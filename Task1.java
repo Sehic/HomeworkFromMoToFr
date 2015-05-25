33210d5a44d2b1e8db0b9f21886770e88f9f7986
unknown <hajrudin.sehic@bitcamp.ba>
2015-05-25 20:15:18 +0200

    Zadatak 1

---
+++ Task1.java
@@ -0,0 +1,16 @@
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		int num = 10, counter = 1, sum = 0;
+		while(counter<num){
+			if(num%counter==0){
+				num/=counter;
+				sum++;
+			}
+			counter++;
+		}
+		System.out.println("Broj se moze podjeliti "+sum+" puta.");
+	}
+
+}
